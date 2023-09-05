609f24264db31fbb02f8f0c5f5c2fd809fcae8b5
MaximeG9 <gurbala.maxime@gmail.com>
2023-09-05 09:25:10 +0200

    add admin file

--- index.php
+++ index.php
@@ -3,6 +3,18 @@
 include_once('./utils/db_connect.php');
 include_once('./utils/loadClass.php');
 include_once('./partials/header.php');
-include_once('./partials/footer.php');
+
 
 ?>
+
+<div class="uk-column-1-2@s">
+    <form class="uk-search uk-search-default">
+        <input class="uk-search-input" type="search" placeholder="" aria-label="">
+        <button type="submit">rechercher</button>
+    </form>
+</div>
+
+
+
+<?php
+include_once('./partials/footer.php');
