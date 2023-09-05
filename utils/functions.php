18bf65e24d5b1355e2959e011d7a61fb0cc49098
VinceS42 <v.fayon.pro@gmail.com>
2023-09-05 13:51:00 +0200

    loginAdmin fisnish

--- utils/functions.php
+++ utils/functions.php
@@ -1,30 +1,31 @@
 <?php
 
-    function login() {
-        
-        include_once('../utils/db_connect.php');
+function login()
+{
+
+    include_once('../utils/db_connect.php');
 
 
-$login = ["Le BeauGoss", "The Boss", "Le Clown"];       
-$password = 'rootRoot';
-password_hash($password, PASSWORD_DEFAULT);
+    $login = ["Le BeauGoss", "The Boss", "Le Clown"];
+    $password = 'rootRoot';
+    password_hash($password, PASSWORD_DEFAULT);
 
 
-if(!empty($_POST['login']) && !empty($_POST['password'])){
-    if(password_verify($_POST['password'], $password) || in_array($_POST['login'], $login)){
-        echo 'connexion success';
-        $_SESSION['pseudo'] = $pseudo; // on enregistre le pseudo dans la session
-        $_SESSION['password'] = $password; // on enregistre le mdp dans la session
-        
-    } else {
-        echo 'login or password wrong';
-    }
-      } else {
-        echo 'user don\'t exist';
-    }
-    
+    if (!empty($_POST['login']) && !empty($_POST['password'])) {
+
+        if (password_verify($_POST['password'], $password) || in_array($_POST['login'], $login)) {
+
+            echo 'connexion success';
+
+            $_SESSION['pseudo'] = $pseudo; // on enregistre le pseudo dans la session
+            $_SESSION['password'] = $password; // on enregistre le mdp dans la session
+
+            header('Location: /admin/dashboard.php');
+        } else {
+            echo 'login or password wrong';
+        }
+    }     
 }
 
 
-?>
-    
+
