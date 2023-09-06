d2ef5635806cf8c2dc3299dac9326716fae32ca7
VinceS42 <v.fayon.pro@gmail.com>
2023-09-06 13:42:45 +0200

    Maj OneOperator

--- utils/db_connect.php
+++ utils/db_connect.php
@@ -3,7 +3,7 @@ $user = 'root';
 $pass = 'root';
 
 try {
-    $db = new PDO('mysql:host=mysql;dbname=tp_operator', $user, $pass);
+    $bdd = new PDO('mysql:host=mysql;dbname=tp_operator', $user, $pass);
 } catch (PDOException $e) {
       print "Erreur ! : " . $e->getMessage() . "<br/>";
       die();
