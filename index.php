c7b368d4d5471ba16174f1ec678010eadcd213d5
MaximeG9 <gurbala.maxime@gmail.com>
2023-09-06 14:26:44 +0200

    almost finished operator list

--- index.php
+++ index.php
@@ -7,16 +7,17 @@ include_once('./partials/header.php');
 
 ?>
 
-
-<form action="./operator-list.php" method="post" class="uk-search uk-search-default">
-    <div>
-        <input class="uk-search-input" type="search" name="search">
-        <input type="hidden" name="id_destination">
-    </div>
-    <div>
-        <button type="submit">rechercher</button>
-    </div>
-</form>
+<div class="uk-container">
+    <form action="./operator-list.php" method="post" class="uk-search uk-search-default">
+        <div>
+            <input class="uk-search-input" type="search" name="search">
+            <input type="hidden" name="id_destination">
+        </div>
+        <div>
+            <button type="submit">rechercher</button>
+        </div>
+    </form>
+</div>
 
 
 
--- operator-list.php
+++ operator-list.php
@@ -12,59 +12,59 @@ $tourOperators = $managerRepo->getOperatorsByDestination($_POST['search']);
 
 
 
-var_dump($tourOperators);
+// var_dump($tourOperators);
 
 
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<section>
 
-<body>
-
-    <?php
-    if (count($tourOperators) > 0) {
-        echo $tourOperators[0]->getDestinations()[0]->getLocation();
-        foreach ($tourOperators as $tourOperator) {
-            $destinations = $tourOperator->getDestinations();
-    ?>
-
-            <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
-                <div class="uk-card-media-left uk-cover-container">
-                    <img src="images/light.jpg" alt="" uk-cover>
-                    <canvas width="600" height="400"></canvas>
-                </div>
-                <div>
-                    <div class="uk-card-body">
-                        <h3 class="uk-card-title"><?= $tourOperator->getName() ?></h3>
-
-                        <?php foreach ($destinations as $destination) { ?>
-                            <!-- // var_dump($destination);  -->
-
-                            <p>Destination : <?= $destination->getLocation() ?></p>
-                            <p>Prix : <?= $destination->getPrice() ?> </p>
-                            <p></p>
-                            <p></p>
-                        <?php } ?>
-                    </div>
+    <div class="uk-container">
+        <?php
+        if (count($tourOperators) > 0) { ?>
+            <div class="uk-flex uk-flex-center">
+                <div class="searchresult uk-padding-small">
+                    <h3><?php echo "Votre recherche pour la destination : " . $tourOperators[0]->getDestinations()[0]->getLocation(); ?></h3>
                 </div>
             </div>
-    <?php }
-    } else {
-        echo "La destination " . $_POST['search'] . " n'est pas proposée.";
-    }
-    ?>
+            <?php foreach ($tourOperators as $tourOperator) {
+                $destinations = $tourOperator->getDestinations();
+            ?>
 
-</body>
 
-</html>
+                <a href="./to.php">
+                    <div class="uk-card uk-card-default uk-card-hover uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
+                        <div class="uk-card-media-left uk-cover-container">
+                            <img src="images/light.jpg" alt="" uk-cover>
+                            <canvas width="600" height="400"></canvas>
+                        </div>
+                        <div>
+                            <div class="uk-card-body">
+                                <h3 class="uk-card-title"><?= $tourOperator->getName() ?></h3>
+
+                                <?php foreach ($destinations as $destination) { ?>
+                                    <!-- // var_dump($destination);  -->
+
+                                    <p>Destination : <?= $destination->getLocation() ?></p>
+                                    <p>Prix : <?= $destination->getPrice() ?> </p>
+                                    <p></p>
+                                    <p></p>
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+        <?php }
+        } else {
+            echo "La destination " . $_POST['search'] . " n'est pas proposée.";
+        }
+        ?>
+    </div>
+
+</section>
+
 
 
 
